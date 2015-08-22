23851077660cdf2ef1d7fb1867dcd8098ba5e8b9
Nancy <nc.nancy.chan@gmail.com>
2015-08-22 13:43:49 -0700

    Assignment 21 completed

---
+++ models/address_book.rb
@@ -0,0 +1,31 @@
+require_relative 'entry.rb'
+
+class AddressBook
+  attr_accessor :entries
+
+  def initialize
+    @entries = []
+  end
+
+  def add_entry(name, phone, email)
+
+    index = 0
+    @entries.each do |entry|
+
+      if name < entry.name
+        break
+      end
+      index += 1
+    end
+
+    @entries.insert(index, Entry.new(name, phone, email))
+  end
+
+  def remove_entry(name, phone, email)
+    @entries.each_with_index do |entry, index|
+      if name == entry.name && phone == entry.phone_number && email == entry.email
+        @entries.delete_at(index)
+      end
+    end
+  end
+end
